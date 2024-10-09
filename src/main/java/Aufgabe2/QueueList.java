ec27e6ad3ef7daf878b3f2650a1bc56e6d7c6e9e
unknown <Bho.Groothoff@btc-ag.com>
2024-10-09 15:33:03 +0200

    popLast method in queueList

--- src/main/java/Aufgabe2/QueueList.java
+++ src/main/java/Aufgabe2/QueueList.java
@@ -3,18 +3,30 @@ package Aufgabe2;
 import java.util.Queue;
 
 public class QueueList implements QueueListInterface {
-
+    int maxSize = 5;
+    int list[] = new int[maxSize];
 
 
 
     @Override
     public int popFront() {
-        return 0;
+        int temp = list[0];//ersten Wert zwischenspeichern
+        for (int i = 0; i < list.length-1; i++) {//alle Werte um einen Platz nach vorne schieben
+            list[i]= list[i+1];
+        }
+        return temp;
     }
 
     @Override
     public int popLast() {
-        return 0;
+        int tempCount=maxSize;
+        while(list[tempCount-1]!=0){
+            
+        }
+        
+        int temp = 0;
+        
+        return temp;
     }
 
     @Override
